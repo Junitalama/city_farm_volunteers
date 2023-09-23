13e3352c2a7b3b71b90c1590ac81d4aee5274891
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-23 23:43:56 +0100

    database created

--- server/server.js
+++ server/server.js
@@ -8,11 +8,16 @@ const bodyParser = require("body-parser");
 // NEW - replace custom middleware with the cors() middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+const { Pool } = require("pg");
+const db = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+});
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  res.send({
-    msg: "Hello, World",
-  });
+  db.query("select * from sessions")
+    .then((result) => res.json(result.rows))
+    .catch((err) => res.send(err));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
