107f41c47c28acae82b2d295a6b8f16939da3825
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-24 02:23:18 +0100

    react calender installed

--- client/farm_volunteers/src/Components/Sessions.jsx
+++ client/farm_volunteers/src/Components/Sessions.jsx
@@ -1,3 +1,27 @@
-import React, {useState.useEffect} from "react";
+import React, {useState,useEffect} from "react";
 
-const Api = 
+const Api = "https://city-farms-db.onrender.com";
+
+function Sessions(){
+    const[session, setSession] = useState([]);
+    
+    useEffect(() => {
+        fetch(Api)
+        .then((res) => res.json())
+        .then((data) => {
+            setSession(data)
+        })
+        .catch((error) => console.error("error"))
+    },[]);
+    return (
+        <div className="card">
+           {session.map((s) => {
+            return (
+              <li key={s.ses_id}>
+                <p>Date :{s.date}</p>
+                <button className={s.status}>{s.status} </button>
+              </li>
+            );})}
+        </div>
+    )
+}export default Sessions;
