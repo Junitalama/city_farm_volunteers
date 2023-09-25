e16bbe4d5cd96d77c0c300df8b6e833e4724f2fb
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-25 22:33:05 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-// NEW - replace custom middleware with the cors() middleware
+
 app.use(cors({ AllowedHeaders: ["Content-Type", "Authorization"] }));
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const { Pool } = require("pg");
 const db = new Pool({
   connectionString: process.env.DB_URL,
@@ -20,21 +21,16 @@ app.get("/", (req, res) => {
     .catch((err) => res.send(err));
 });
 
-// app.get("/volunteers",(req, res) =>{
-//   db.query("select * from volunteers where slot ilike '%booked%'")
-//     .then((result) => res.json(result.rows))
-//     .catch((err) => res.send(err));
-// })
-
+app.get("/volunteers", (req, res) => {
+  db.query("select * from volunteers")
+    .then((result) => res.json(result.rows))
+    .catch((err) => res.send(err));
+});
 
 app.get("/volunteers/:slot", (req, res) => {
   let slotLooked = req.params.slot;
-  
-    db.query(
-      "select * from volunteers where slot like $1 || '%'",
-      [slotLooked])
-    
-      .then((result) => res.json(result.rows))
-      .catch((err) => res.json(err));
-  })
+  db.query("select * from volunteers where slot like $1 || '%'", [slotLooked])
+    .then((result) => res.json(result.rows))
+    .catch((err) => res.json(err));
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
