02547aeb13585569d1f076da35281cd91a351a52
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-09-26 23:35:02 +0100

    form component created

---
+++ client/farm_volunteers/src/Components/Form.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const Form = ({ onAdd }) => {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [phone, setPhone] = useState("");
+const [date, setDate] = useState("");
+    
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name,email,phone && date) {
+      onAdd({ name, email,phone,date });
+      setName("");
+      setEmail("");
+      setPhone("");
+      setDate("")
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <p className="add">Add your videos </p>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="tel"
+          placeholder="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="date"
+          placeholder="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <button type="submit">Add </button>
+    </form>
+  );
+};
+
+export default Form;
