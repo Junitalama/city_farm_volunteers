e4ce20ccc74112f76ab4d4e4b48862493382ccbb
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-02 18:13:30 +0100

    contact component created

---
+++ client/farm_volunteers/src/Components/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h4>Join us on:</h4>
+      <div className="contact" id="contact">
+        <img
+          class="contact-logo"
+          src="https://raw.githubusercontent.com/CodeYourFuture/HTML-CSS-Module-Project/3582b6b7a0fef7533856c51feef4eba1cc20adeb/img/twitter-icon.svg"
+          alt="twitter-logo"
+          width="40"
+        ></img>
+        <img
+          class="contact-logo"
+          src="https://raw.githubusercontent.com/CodeYourFuture/HTML-CSS-Module-Project/2eafd55a92f7931f2ce6d3264a4ea98a9488dc7e/img/facebook-icon.svg"
+          alt="facebook logo"
+          width="40"
+        ></img>
+        <img
+          class="contact-logo"
+          src="https://raw.githubusercontent.com/CodeYourFuture/HTML-CSS-Module-Project/2eafd55a92f7931f2ce6d3264a4ea98a9488dc7e/img/instagram-icon.svg"
+          alt="instragram logo"
+          width="40"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
