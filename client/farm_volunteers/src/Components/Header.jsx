095cda889b49587af78c3194b3b08a13e6c0772f
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-02 18:12:56 +0100

    header component created

---
+++ client/farm_volunteers/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const Header = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div">
+            City-Farm
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default Header;
