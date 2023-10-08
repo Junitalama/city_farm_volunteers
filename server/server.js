eb45cc0ba2ecc6645f026ee71ac24d9e7e5e0069
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-10-08 01:10:57 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get("/volunteers", (req, res) => {
 app.post("/booking", (req, res) => {
   const { date, slot,status, name, email, phone } = req.body;
   db.query(
-    "insert into bookings (ses_id, vol_id) values ((select ses_id from sessions where date = $1 and slot = $2 and status = $3), (select vol_id from volunteers where name = $4 and email = $5 and phone = $6)) returning *",
+    "insert into bookings (ses_id, vol_id) values ((select ses_id from sessions where date = $1 and slot = $2 and status = $3), (select vol_id from volunteers where name = $4 and email = $5 and phone = $6)) returning booking_id",
     [date, slot, status, name, email, phone]
   )
     .then((result) => {
