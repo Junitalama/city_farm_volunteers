00c4e32b0c4aba222508099ec1b7518b9548cbf3
Junitalama <113072416+Junitalama@users.noreply.github.com>
2024-01-25 17:06:29 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const bodyParser = require("body-parser");
 app.use(cors({ AllowedHeaders: ["Content-Type", "Authorization"] }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +15,7 @@ const db = new Pool({
   ssl: { rejectUnauthorized: false},
 });
 app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
   db.query("select * from sessions")
