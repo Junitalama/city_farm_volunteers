68471f916c7eea67fdc7ac79f4267cc2a1982ee0
Junitalama <113072416+Junitalama@users.noreply.github.com>
2024-01-31 18:28:05 +0000

    test stage

--- frontend/farm_volunteers/src/App.js
+++ frontend/farm_volunteers/src/App.js
@@ -1,37 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Volunteers from "./Components/Volunteers";
-import ImgMediaCard from "./Components/Card";
-import Header from "./Components/Header";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
-import CustomizedMenus from "./Components/ViewOption";
-import { Switch, Route } from "react-router-dom";
 import BookSession from "./Components/BookSession";
-
-
+import Contact from "./Components/Contact";
+import CustomizedMenus from "./Components/ViewOption";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import ImgMediaCard from "./Components/Card";
+import Volunteers from "./Components/Volunteers";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={ImgMediaCard}></Route>
-    <Route exact path="/manager" component={Volunteers}></Route>
-    <Route exact path="/volunteer" component={BookSession}></Route>
+    <Route exact path="/" component={ImgMediaCard} />
+    <Route exact path="/manager" component={Volunteers} />
+    <Route exact path="/volunteer" component={BookSession} />
   </Switch>
 );
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <CustomizedMenus />
-      <Main />
-      
-      
-
-      <Contact />
-
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <CustomizedMenus />
+        <Main />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
