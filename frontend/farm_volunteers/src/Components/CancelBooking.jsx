be6a374670cfc36a3e63c715b844620eb9f50b5d
Junitalama <113072416+Junitalama@users.noreply.github.com>
2024-02-04 12:10:07 +0000

    Update CancelBooking.jsx

--- frontend/farm_volunteers/src/Components/CancelBooking.jsx
+++ frontend/farm_volunteers/src/Components/CancelBooking.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 
 function CancelBookingButton({ bookingId, onDelete }) {
-  const [showConfirmation, setShowConfirmation] = useState(false);
-
   const deleteBooking = () => {
     fetch(
       `http://ec2-3-9-146-224.eu-west-2.compute.amazonaws.com:5000/bookings/${bookingId}`,
@@ -16,36 +14,13 @@ function CancelBookingButton({ bookingId, onDelete }) {
           onDelete(bookingId);
         }
       })
-      .catch((error) => {
-        console.error("Error deleting booking:", error);
-        // Handle error and provide user feedback
-      });
-  };
-
-  const handleCancelConfirmation = () => {
-    setShowConfirmation(false);
-    deleteBooking();
+      .catch((error) => console.error("Error deleting booking:", error));
   };
 
   return (
-    <>
-      <button
-        className="btn btn-danger"
-        onClick={() => setShowConfirmation(true)}
-      >
-        Cancel Booking
-      </button>
-
-      {showConfirmation && (
-        <div className="modal">
-          <div className="modal-content">
-            <p>Are you sure you want to cancel this booking?</p>
-            <button onClick={handleCancelConfirmation}>Yes</button>
-            <button onClick={() => setShowConfirmation(false)}>No</button>
-          </div>
-        </div>
-      )}
-    </>
+    <button className="btn btn-danger" onClick={deleteBooking}>
+      Cancel Booking
+    </button>
   );
 }
 
