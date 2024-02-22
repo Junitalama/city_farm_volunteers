72d675eac63f90e45a42888643e3cd340adbcb95
Junita Lama <lamajunitaa@gmail.com>
2024-02-22 22:35:18 +0000

    Create main.tf
    database deplyoed to rds using terraform

---
+++ rds/main.tf
@@ -0,0 +1,22 @@
+
+
+provider "aws" {
+  region = "eu-west-2"  
+}
+
+resource "aws_db_instance" "junita" {
+  # Placeholder values, replace them with your RDS configuration
+  identifier            = "junita"
+  allocated_storage     = 20 
+  engine                = "postgres"
+  engine_version        = "15.5"
+  instance_class        = "db.t3.micro"
+  db_name               = "junita_lama"
+  username              = "postgres"
+  password              = "password"
+  parameter_group_name  = "default.postgres15"
+  publicly_accessible   = true
+
+  vpc_security_group_ids = ["sg-07dc9cff6004048cb"]  
+ 
+}
